b8b36099805bb8bb5deb3c76a7e8b889b1a29d53
Kanwaraj <kanwarajcool@gmail.com>
2024-12-01 14:47:46 +0530

    Intalled shadCn ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 
-export default function App() {
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+      <p>fdsfsd</p>
+      <Button className="bg-red-400" varient="ghost">Click Me</Button>
+    </div>
   );
-}
+};
+
+export default App;
